d509bbdf2a4dee2796fa2c8a4e536cca1c78fb7e
patsongco <pat.songco@gmail.com>
2022-06-13 14:28:06 +1000

    initial commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,7 +11,6 @@ class About extends Component {
     const street = this.props.data.address.street;
     const city = this.props.data.address.city;
     const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
     const resumeDownload = this.props.data.resumedownload;
